32d36ef7b3c2a5c156547364aaccf7e564f1052d
Manenga Mungandi <mungandm@hotmail.com>
2019-03-11 17:46:04 +0200

    Initial Commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Manenga Mungandi on 2019/03/11.
+//  Copyright © 2019 Manenga Mungandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
